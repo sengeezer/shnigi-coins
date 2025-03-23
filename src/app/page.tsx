3ad40c1ad8f4c336c1cdba0f7d9eb6db109a1e88
Fabio Albertin <albertin@gmail.com>
2025-03-23 01:17:32 +0000

    Adding content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,42 @@
-import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Divider from '@mui/material/Divider';
 import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
 import Grid from '@mui/material/Grid2';
+import Typography from '@mui/material/Typography';
+
 type Props = { children: React.ReactNode }
 const Item = ({children}: Props) => (<Paper>{children}</Paper>)
 
 export default function Home() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Container maxWidth="lg">
       <Grid container spacing={2}>
         <Grid size={8}>
-          <Item>size=8</Item>
+          <Typography variant="h1" gutterBottom>Shnigi Coins</Typography>
         </Grid>
-        <Grid size={4}>
-          <Item>size=4</Item>
+        <Grid size={6}>
+          <Item>
+          <Typography variant="h3" gutterBottom>Details</Typography>
+            <Stack
+              direction="row"
+              divider={<Divider orientation="vertical" flexItem />}
+              spacing={2}
+            >
+              <Item>Contact address:</Item>
+              <Item>Ticker:</Item>
+              <Item>Tax:</Item>
+              <Item>LP:</Item>
+            </Stack>
+          </Item>
         </Grid>
-        <Grid size={4}>
-          <Item>size=4</Item>
+        <Grid size={2}>
+          <Item>size=2</Item>
         </Grid>
         <Grid size={8}>
-          <Item>size=8</Item>
+          <Item>&copy; 2025 Shnigi Coins</Item>
         </Grid>
       </Grid>
-    </Box>
+    </Container>
   );
 }
